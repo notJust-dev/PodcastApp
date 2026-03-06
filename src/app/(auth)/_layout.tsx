572b3efa2c60ce4fc2bc2bad3476bcad206cc13a
Vadim Savin <savinvadim1312@gmail.com>
2026-03-06 16:12:53 +0100

    authentication using Clerk:

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name='sign-in' options={{ title: "Sign in" }} />
+      <Stack.Screen name='sign-up' options={{ title: "Sign up" }} />
+    </Stack>
+  )
+}
